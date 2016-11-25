90a7e51c4aa248e38bce6a0d465fa0aad7cca490
Etienne Curati <etienne.curati@gmail.com>
2016-11-25 16:42:02 -0200

    Funcutional site but image doesn't work

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+class PhotoUploader < CarrierWave::Uploader::Base
+
+  include Cloudinary::CarrierWave
+  version :standard do
+    cloudinary_transformation width: 300, height: 250, crop: :fill
+  end
+
+  version :landscape do
+    cloudinary_transformation width: 1200, height: 500,  crop: :fill
+  end
+end
